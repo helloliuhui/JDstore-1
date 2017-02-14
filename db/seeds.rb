903864a6bc8e464900cab4e4e52c61026a425dab
liuhui <liuhuijisuanji@gmail.com>
2017-02-14 14:55:09 +0800

    delete seed products

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,3 @@ u.password = "111111"
 u.password_confirmation = "111111"
 u.is_admin = true
 u.save
-
-
-products_categories = ["Giant", "Trek"]
-create_products = for i in 1..5 do
-Product.create!([title: products[i - 1],
-description: products_description[i - 1],
-quantity: rand(0..10),
-price: products_prices[i - 1],
-image: File.open(File.join(Rails.root, products_images[i-1])),
-categories: products_categories[i-1] ])
-end
